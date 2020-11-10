b88b8ca07441059ab91d872847b86ac1d64af25c
JozanLeClerc <bousset.rudy@gmail.com>
2020-11-10 13:19:56 +0100

    Removed swallow patch

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
@@ -21,6 +21,8 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"/home/jozan/.config/dwm/dwmrc", NULL,
+	"/usr/local/bin/notify-send", "-u", "normal", "\uf30c", "Welcome back, partner!", NULL,
+	"/usr/local/bin/espeak", "Welcome back, partner!", NULL,
 	NULL /* terminate */
 };
 
@@ -32,12 +34,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class         instance  title             tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",        NULL,     NULL,             0,         1,          0,           0,        -1 },
-	{ "Firefox",     NULL,     NULL,             1 << 2,    0,          0,          -1,        -1 },
-	{ "st-256color", NULL,     NULL,             0,         0,          1,           0,        -1 },
-	{ NULL,          NULL,     "DergodsRealmII", 0,         1,          0,           1,        -1 },
-	{ NULL,          NULL,     "Event Tester",   0,         0,          0,           1,        -1 }, /* xev */
+	/* class         instance  title             tags mask  isfloating  monitor */
+	{ "Firefox",     NULL,     NULL,             1 << 2,    0,          -1 },
+	{ "st-256color", NULL,     NULL,             0,         0,          -1 },
+	{ NULL,          NULL,     "DergodsRealmII", 0,         1,          -1 },
+	{ NULL,          NULL,     "Event Tester",   0,         0,          -1 }, /* xev */
 };
 
 /* layout(s) */
