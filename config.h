a6ea424efa959e91353bdafe24551b82429a95da
Joe <bousset.rudy@gmail.com>
2022-09-19 14:37:19 +0200

    up

--- config.h
+++ config.h
@@ -92,7 +92,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *term_cmd[]        = { "alacritty", NULL };
+static const char *term_cmd[]        = { "st", NULL };
 static const char *coolterm_cmd[]    = { "cool-retro-term", NULL };
 static const char *dmenu_cmd[]       = { "dmenu_run", "-i", "-m", "0", NULL };
 static const char *dmpc_cmd[]        = { "/home/jozan/.local/bin/dmpc", NULL };
