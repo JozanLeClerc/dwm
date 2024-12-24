b450e840e1d6decda88cdfd71da9151a81ea9396
Joe <rbo@gmx.us>
2024-12-24 22:20:55 +0100

    up

--- config.def.h
+++ config.def.h
@@ -14,7 +14,9 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "NotoSansMono Nerd Font:size=13" };
+static const char *fonts[]          = { "BigBlueTermPlus Nerd Font:size=11" };
+// static const char *fonts[]          = { "Terminess Nerd Font:size=13" };
+// static const char *fonts[]          = { "GohuFont 11 Nerd Font:size=13" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#1d2021"; /* bar background */
 static const char col_gray2[]       = "#32302f"; /* last square, windows borders */
