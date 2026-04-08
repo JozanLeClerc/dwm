9480c51b716d235d4970559fbd85f23043102719
joe <rbo@gmx.us>
2026-04-08 12:27:19 +0200

    refresh

--- config.def.h
+++ config.def.h
@@ -12,7 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int refreshrate        = 144;      /* Update rate for drag and resize events, in updates (frames) per second */
+static const int user_bh            = 30;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "BlexMono Nerd Font:style=Medium:size=13" };
 // static const char *fonts[]          = { "MonaspiceNe Nerd Font:size=12" };
