846c2a0e7ceb1c4c0292e68d520c5a06fb8b15ce
joe <rbo@gmx.us>
2026-06-13 22:02:54 +0200

    up

--- dwm.c
+++ dwm.c
@@ -1144,7 +1144,8 @@ drawbar(Monitor *m)
 {
 	int x, w, tw = 0, stw = 0;
 	// int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
+	// int boxw = drw->fonts->h / 6 + 2;
+	int boxw = drw->fonts->h / 4 + 2;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
