90949c180991034f7a7eaf6bca23ce662743adb8
rudy.bousset <rudy.bousset@leviia.com>
2026-06-17 14:48:58 +0200

    up

--- dwm.c
+++ dwm.c
@@ -284,7 +284,7 @@ static void tile(Monitor *m);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggleforegrounded(const Arg *arg);
-// static void togglefullscr(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void togglescratch(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
@@ -3015,12 +3015,12 @@ toggleforegrounded(const Arg *arg)
 	arrange(selmon);
 }
 
-/* void
+void
 togglefullscr(const Arg *arg)
 {
 	if(selmon->sel)
 		setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
-} */
+}
 
 void
 togglescratch(const Arg *arg)
